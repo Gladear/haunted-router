6b4350dff08a5a791aee19df4423f1cf1b5021e5
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-09-07 20:05:30 +0200

    test: Add tests for router-link

---
+++ test/test-router-link.js
@@ -0,0 +1,93 @@
+import { navigateTo } from '../haunted-router.js';
+import { cycle, waitPopState } from './helpers.js';
+
+function attachLink(attributes) {
+  const el = document.createElement('a', { is: 'router-link' });
+  Object.assign(el, attributes);
+
+  host.appendChild(el);
+  return [el, () => host.removeChild(el)];
+}
+
+describe('router-link', () => {
+  it('Mounts', async () => {
+    try {
+      const [, teardown] = attachLink({});
+      await cycle();
+
+      teardown();
+    } catch {
+      assert.fail('Link failed to mount');
+    }
+  });
+
+  it('Navigates to the url', () => {
+    const url = '/link-navigation-test';
+    const [el, teardown] = attachLink({
+      href: url,
+    });
+
+    el.click();
+
+    assert.strictEqual(location.pathname, url, 'Current location is incorrect');
+
+    teardown();
+  });
+
+  it('Defines the state', () => {
+    const url = '/link-state-test';
+    const state = 'link-state-test';
+    const [el, teardown] = attachLink({
+      href: url,
+      state,
+    });
+
+    el.click();
+
+    assert.strictEqual(history.state, state, 'State is not defined');
+
+    teardown();
+  });
+
+  it('Creates an entry in the history', async () => {
+    const { pathname: original } = location;
+    const url = '/link-history-entry-test';
+    const [el, teardown] = attachLink({
+      href: url,
+    });
+
+    el.click();
+
+    const popState = waitPopState();
+    history.back();
+
+    await popState;
+
+    assert.strictEqual(location.pathname, original, 'No entry were created in the history');
+
+    teardown();
+  });
+
+  it('Doesn\'t create an history entry with `replace`', async () => {
+    const { pathname: original } = location;
+    const secondUrl = '/url-entry-test';
+
+    const [el, teardown] = attachLink({
+      href: secondUrl,
+      replace: true,
+    });
+
+    navigateTo('/temporary-url');
+
+    el.click();
+
+    const popState = waitPopState();
+    history.back();
+
+    await popState;
+
+    assert.strictEqual(location.pathname, original, 'An entry was created in the history');
+
+    teardown();
+  });
+});
