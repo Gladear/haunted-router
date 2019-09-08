6ee2273fa8c7cafeae5d5ef42d347d74c3b47b89
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-09-08 16:04:55 +0200

    test(router-redirect): create tests

---
+++ test/test-router-redirect.js
@@ -0,0 +1,68 @@
+import { component, html } from 'https://unpkg.com/haunted@beta/haunted.js';
+import { navigateTo } from '../haunted-router.js';
+import { attach, cycle, waitPopState } from './helpers.js';
+
+describe('router-redirect', () => {
+  it('Redirects to the url', async () => {
+    const tag = 'redirect-url-test';
+    const url = '/redirect-url-test';
+
+    function App() {
+      return html`<router-redirect .url=${url} />`;
+    }
+    customElements.define(tag, component(App));
+
+    const teardown = attach(tag);
+    await cycle();
+
+    assert.equal(location.pathname, url, 'The location is incorrect');
+
+    teardown();
+  });
+
+  it('Creates an entry in the history', async () => {
+    const tag = 'history-entry-redirect-test';
+    const { pathname: original } = location;
+
+    function App() {
+      return html`<router-redirect .url=${'/redirect-entry-url-test'} />`;
+    }
+    customElements.define(tag, component(App));
+
+    const teardown = attach(tag);
+    await cycle();
+
+    let popState = waitPopState();
+    history.back();
+
+    await popState;
+
+    assert.equal(location.pathname, original, 'It did not create an history entry');
+
+    teardown();
+  });
+
+  it('Doesn\'t create an history entry with `replace`', async () => {
+    const tag = 'replace-entry-redirect-test';
+    const { pathname: original } = location;
+
+    function App() {
+      return html`<router-redirect ?replace=${true} .url=${'/replace-entry-redirect-test'} />`;
+    }
+    customElements.define(tag, component(App));
+
+    navigateTo('/temporary-url');
+
+    const teardown = attach(tag);
+    await cycle();
+
+    let popState = waitPopState();
+
+    history.back();
+    await popState;
+
+    assert.equal(location.pathname, original, 'It did create an entry in the history');
+
+    teardown();
+  });
+});
